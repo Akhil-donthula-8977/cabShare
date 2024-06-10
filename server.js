018a8c74807b9688080f6050767a7d4f94685de1
Akhil <akhil.donthula8977@gmail.com>
2024-06-10 17:11:09 +0530

    deploy  bug fix

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const next = require('next');
 const { Server } = require('socket.io');
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
-const port = 3000;
+const hostname = '0.0.0.0';
+const port = process.env.PORT || 3000;
 
 const app = next({ dev });
 const handler = app.getRequestHandler();
@@ -17,41 +17,33 @@ app.prepare().then(() => {
     console.log('A client connected');
 
     socket.on('notify', (data) => {
-      console.log("check datas",data)
+      console.log("check data", data);
       const recipientSocket = io.sockets.sockets.get(data.socketID);
       if (!recipientSocket) {
         console.error('Recipient socket not found');
         return;
       }
-        console.log("crt")
+      console.log("crt");
       io.to(data.socketID).emit("requestNotification", data);
-    
-    })
-    
-    socket.on("requestAccept",(data)=>{
-      console.log("socket check",data)
-      io.to(data.socketId).emit("requestAccepted",data)
-    })
+    });
+
+    socket.on("requestAccept", (data) => {
+      console.log("socket check", data);
+      io.to(data.socketId).emit("requestAccepted", data);
+    });
 
     socket.on('sendMessage', (message) => {
-      console.log("polo")
+      console.log("polo");
       io.to(message.receiver).emit('message', message);
-
     });
+
     socket.on("test", () => {
-      console.log("hello world")
-    })
+      console.log("hello world");
+    });
+
     socket.on('disconnect', () => {
       console.log('A client disconnected');
     });
-
-
-
-  });
-
-  httpServer.once('error', (err) => {
-    console.error(err);
-    process.exit(1);
   });
 
   httpServer.listen(port, hostname, () => {
